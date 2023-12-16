8ae2fbd9736116106e41578997a127ff6917435f
illyakoynash <illyakoynash@gmail.com>
2023-12-16 10:44:03 +0200

    .

--- dz15_js.js
+++ dz15_js.js
@@ -9,4 +9,17 @@ document.getElementById('MyForm').addEventListener('submit', function(event) {
     var password2 = document.getElementById('PasswordInput_check').value;
     console.log('Password: ' + password2)
 
-})
+})          
+
+function f() {
+    var name = document.getElementById('nameInput').value;
+    document.getElementById("result1").innerText='Your name  '+name
+    var prizvyshe = document.getElementById('PrizvysheInput').value;
+    document.getElementById("result2").innerText='Your pr  '+prizvyshe
+    var password = document.getElementById('PasswordInput').value;
+    document.getElementById("result3").innerText='Your password  '+password
+    var password2 = document.getElementById('PasswordInput_check').value;
+    document.getElementById("result4").innerText='Your password  '+password
+    
+}
+
